a555b94cb6f939a068cc9d29f6938fd8c553f91e
Princic-1837592 <andreapri.99@gmail.com>
2024-10-11 11:56:24 +0200

    to do frontend

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 import init, {
-	// apply,
+	apply,
 } from "./wasm/pkg/wasm.js";
 
 window.onload = async function () {
 	await init();
+	const canvas = document.createElement("CANVAS");
+	const width = 10;
+	const height = 10;
+	canvas.width = width - (width % 2);
+	canvas.height = height - (height % 4);
+
+	const ctx = canvas.getContext("2d");
+	ctx.fillStyle = "#ffffffff";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	const bytes = new Uint8Array(ctx.getImageData(0, 0, canvas.width, canvas.height).data.buffer);
+	try {
+		apply(bytes, canvas.width - 1, false, true, 1, false)
+	} catch (e) {
+		console.log(typeof e);
+	}
 };
